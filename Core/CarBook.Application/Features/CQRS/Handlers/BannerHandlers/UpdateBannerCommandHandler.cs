a8f790f6335b4d709e0e986a720134264aa5c437
RFatih <39864827+R-Fatih@users.noreply.github.com>
2023-12-08 11:28:22 +0300

    banner cqrs

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/BannerHandlers/UpdateBannerCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.CQRS.Commands.BannerCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.BannerHandlers
+{
+	public class UpdateBannerCommandHandler
+	{
+		private readonly IRepository<Banner> _repository;
+
+		public UpdateBannerCommandHandler(IRepository<Banner> repository)
+		{
+			_repository = repository;
+		}
+		public async Task Handle(UpdateBannerCommand updateBannerCommand)
+		{
+			await _repository.UpdateAsync(new Banner
+			{
+				Description = updateBannerCommand.Description,
+				VideoURL = updateBannerCommand.VideoURL,
+				VideoDescription = updateBannerCommand.VideoDescription,
+				Title = updateBannerCommand.Title,
+				BannerId=updateBannerCommand.BannerId
+			});
+		}
+	}
+}
---
+++ Presentation/CarBook.WebApi/Controllers/BannersController.cs
@@ -0,0 +1,58 @@
+﻿using CarBook.Application.Features.CQRS.Commands.BannerCommands;
+using CarBook.Application.Features.CQRS.Handlers.BannerHandlers;
+using CarBook.Application.Features.CQRS.Queries.BannerQueries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BannersController : ControllerBase
+	{
+		private readonly CreateBannerCommandHandler _createCommandHandler;
+		private readonly GetBannerByIdQueryHandler _getBannerByIdQueryHandler;
+		private readonly GetBannerQueryHandler _getBannerQueryHandler;
+		private readonly UpdateBannerCommandHandler _updateBannerCommandHandler;
+		private readonly RemoveBannerCommandHandler _removeBannerCommandHandler;
+
+		public BannersController(CreateBannerCommandHandler createCommandHandler, GetBannerByIdQueryHandler getBannerByIdQueryHandler, GetBannerQueryHandler getBannerQueryHandler, UpdateBannerCommandHandler updateBannerCommandHandler, RemoveBannerCommandHandler removeBannerCommandHandler)
+		{
+			_createCommandHandler = createCommandHandler;
+			_getBannerByIdQueryHandler = getBannerByIdQueryHandler;
+			_getBannerQueryHandler = getBannerQueryHandler;
+			_updateBannerCommandHandler = updateBannerCommandHandler;
+			_removeBannerCommandHandler = removeBannerCommandHandler;
+		}
+		[HttpGet]
+		public async Task<IActionResult> BannerList()
+		{
+			return Ok(await _getBannerQueryHandler.Handle());
+		}
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetBanner(int id)
+		{
+			var value = await _getBannerByIdQueryHandler.Handle(new GetBannerByIdQuery(id));
+			return Ok(value);
+
+		}
+		[HttpPost]
+		public async Task<IActionResult> CreateBanner(CreateBannerCommand command)
+		{
+			await _createCommandHandler.Handle(command);
+			return Ok("Banner bilgisi eklendi.");
+		}
+		[HttpDelete]
+		public async Task<IActionResult> RemoveBanner(int id)
+		{
+			await _removeBannerCommandHandler.Handle(new RemoveBannerCommand(id));
+			return Ok("Banner bilgisi silindi.");
+		}
+		[HttpPut]
+		public async Task<IActionResult> UpdateBanner(UpdateBannerCommand command)
+		{
+			await _updateBannerCommandHandler.Handle(command);
+			return Ok("Banner bilgisi güncellendi.");
+		}
+	}
+}
