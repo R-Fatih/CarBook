cc7417cf7d3ae630cd49f1601efac5d9d0b80b55
RFatih <39864827+R-Fatih@users.noreply.github.com>
2023-12-12 20:06:05 +0300

    up

---
+++ Presentation/CarBook.WebApi/Controllers/CommentsController.cs
@@ -0,0 +1,50 @@
+﻿using CarBook.Application.Features.RepositoryPattern;
+using CarBook.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly IGenericRepository<Comment> _repository;
+
+        public CommentsController(IGenericRepository<Comment> repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public IActionResult GetCommentList()
+        {
+            var values=_repository.GetAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateComment(Comment comment)
+        {
+            _repository.Create(comment);
+            return Ok("Başarıyla eklendi");
+        }
+        [HttpPut]
+        public IActionResult UpdateComment(Comment comment)
+        {
+            _repository.Update(comment);
+            return Ok("Başarıyla güncellendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _repository.GetById(id);
+            _repository.Remove(value);
+            return Ok("Başarıyla silindi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var values = _repository.GetById(id);
+            return Ok(values);
+        }
+    }
+}
