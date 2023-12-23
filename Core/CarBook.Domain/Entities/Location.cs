c138aa7301474fd64d1d1162904c751e8298c990
RFatih <39864827+R-Fatih@users.noreply.github.com>
2023-12-23 23:27:38 +0300

    reservation entity

--- Core/CarBook.Domain/Entities/Location.cs
+++ Core/CarBook.Domain/Entities/Location.cs
@@ -11,7 +11,8 @@ namespace CarBook.Domain.Entities
         public int LocationId { get; set; }
 		public string Name { get; set; }
         public List<RentACar> RentACars { get; set; }
+        public List<Reservation> PickUpReservations { get; set; }
+        public List<Reservation> DropOffReservations { get; set; }
       
-
     }
 }
---
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -0,0 +1,39 @@
+﻿using CarBook.Dtos.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ReservationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Araç kiralama";
+            ViewBag.v2 = "Araç Rezervasyon Formu";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7224/api/Locations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+                List<SelectListItem> values2 = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.LocationId.ToString(),
+
+                                                }).ToList();
+                ViewBag.v = values2;
+                return View();
+            }
+            return View();
+        }
+    }
+}
